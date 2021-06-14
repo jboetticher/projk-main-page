7ee0b6416880947238d30e9ea2730df9a0f5cfd3
Kevin Xu <bobthehoboofhobos@gmail.com>
2021-06-13 17:21:14 -0700

    now added kevin page

--- src/pages/kevin.js
+++ src/pages/kevin.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import KInfo from '../info/k-projk.json';
 import GlitchTitle from '../components/GlitchTitle';
-import '../css/k-theme.css';
+import ProjectCard from '../components/ProjectCard';
+import '../css/j-theme.css';
 
-export default () => (
-  <>
-    <div style={{ textAlign: 'center' }}>
-      <GlitchTitle text="KEVIN" />
-    </div>
-  </>
-);
+let KevinPage = props => {
+
+    console.log(KInfo.main);
+  
+    return (
+      <>
+        <div style={{ height: "250px" }}>
+          <div style={{ textAlign: 'center' }}>
+            <GlitchTitle text="KEVIN" />
+          </div>
+        </div>
+        <div className="row">
+          {KInfo.main.map(projk => (
+            <div className="col-12 col-md-6 col-lg-4">
+              <ProjectCard
+                jeremy
+                {...projk}
+              />
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
+  
+  export default KevinPage;
