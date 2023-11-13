98152c7eb5a38b76beb3ab8921356c6d611b5570
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-11-13 00:03:16 -0600

    Moved page transition into its own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,20 @@
 import styles from './styles/page.module.css';
 
 import SquircleBackdrop from './components/SquircleBackdrop';
+import PageTransition from './components/PageTransition';
 import glitchStyle from './styles/glitch.module.css';
 
 export default function Home() {
 
 
-  const PageTransition = <div className={styles.loadingAnimationContainer}>
-    <div className={styles.blackOut} />
-    <div className={`${styles.diagonalLine} ${styles.purple} ${styles.animate}`}></div>
-    <div className={`${styles.diagonalLine} ${styles.green} ${styles.animate}`}></div>
-  </div>;
-
   return (
     <main style={{ overflow: 'hidden' }}>
-      {PageTransition}
+      <PageTransition />
       <SquircleBackdrop />
-      <div className={glitchStyle.glitch} data-text={"PROJK"} style={{ width: '323px' }}>
-        PROJK
+      <div className={styles.center}>
+        <h1 className={glitchStyle.glitch} data-text={"PROJK"} style={{ width: '323px' }}>
+          PROJK
+        </h1>
       </div>
     </main>
   );
