4606db929ab395b00e56ffc085016df06f4dae41
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-11-16 18:44:22 -0600

    Fixed animation clipping

--- src/app/components/PageTransition.tsx
+++ src/app/components/PageTransition.tsx
@@ -3,10 +3,12 @@ import styles from "../styles/page-transition.module.css";
 export default function SquircleBackdrop() {
 
     return (
-        <div className={styles.loadingAnimationContainer}>
-            <div className={styles.blackOut} />
-            <div className={`${styles.diagonalLine} ${styles.purple} ${styles.animate}`}></div>
-            <div className={`${styles.diagonalLine} ${styles.green} ${styles.animate}`}></div>
+        <div className={styles.clipContainer}>
+            <div className={styles.loadingAnimationContainer}>
+                <div className={styles.blackOut} />
+                <div className={`${styles.diagonalLine} ${styles.purple} ${styles.animate}`}></div>
+                <div className={`${styles.diagonalLine} ${styles.green} ${styles.animate}`}></div>
+            </div>
         </div>
     )
 }
