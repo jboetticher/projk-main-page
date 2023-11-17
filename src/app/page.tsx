1fb062b059d342e4966e4edebb6b9799b5c3458f
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-11-16 19:13:54 -0600

    Added links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,34 @@ import glitchStyle from './styles/glitch.module.css';
 
 export default function Home() {
 
+  const links = [
+    { link: '#', title: 'Portfolio' },
+    { link: '#', title: 'Writings' },
+    { link: '#', title: 'Achievements' },
+  ];
 
   return (
     <main style={{ overflow: 'hidden' }}>
       <PageTransition />
       <SquircleBackdrop />
       <div className={styles.center}>
-        <h1 className={glitchStyle.glitch} data-text={"PROJK"} style={{ width: '323px' }}>
+        <h1 className={glitchStyle.glitch} data-text={"PROJK"} style={{ width: '323px', fontSize: '100px' }}>
           PROJK
         </h1>
+        <p className={styles.subtitle}>I'm Jeremy. I have projects.</p>
+        <div className={styles.linkContainer}>
+          {links.map((link, index) => (
+            <a
+              key={index}
+              href={link.link}
+              className={`${glitchStyle.glitch} ${styles.link}`}
+              style={{ animationDelay: `${0.8 + index * 0.5}s` }}
+              data-text={link.title}
+            >
+              {link.title}
+            </a>
+          ))}
+        </div>
       </div>
     </main>
   );
