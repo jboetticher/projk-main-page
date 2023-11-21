e5053f88e0636bd9af5e407eeec1dcbd958f23ec
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-11-21 15:41:51 -0600

    Some styling

--- app/portfolio/[id]/page.tsx
+++ app/portfolio/[id]/page.tsx
@@ -1,8 +1,22 @@
 import { sql } from "@vercel/postgres";
 import { Grid } from "@mui/material";
 import { Tag } from "../_components/Tag";
-import glitchStyle from "../../_styles/glitch.module.css";
 import { MDXRemote } from 'next-mdx-remote/rsc'
+
+import glitchStyle from "../../_styles/glitch.module.css";
+import style from "./page.module.css";
+import { JSX, ClassAttributes, HTMLAttributes } from "react";
+import PageTransition from "../../_components/PageTransition";
+import Link from "next/link";
+
+const MDXComponents = {
+  h1: (props: JSX.IntrinsicAttributes & ClassAttributes<HTMLHeadingElement> & HTMLAttributes<HTMLHeadingElement>) => (
+    <h1 {...props} className={style['mkdwn-h1']}>{props.children}</h1>
+  ),
+  p: (props: JSX.IntrinsicAttributes & ClassAttributes<HTMLHeadingElement> & HTMLAttributes<HTMLHeadingElement>) => (
+    <p {...props} className={style['mkdwn-p']}>{props.children}</p>
+  ),
+};
 
 type ProjectQuery = {
   id: string;
@@ -11,25 +25,34 @@ type ProjectQuery = {
   tagline: string;
   images: string[];
   description: string;
-}
+};
 
 const ProjectPage = async ({ params }: { params: { id: string } }) => {
   const data: ProjectQuery = (await sql`SELECT * FROM projects WHERE id = ${params.id}`).rows[0] as ProjectQuery;
 
+  // TODO: replace Grid with static elements
+
   return (
-    <main style={{ overflow: 'hidden' }}>
-      <Grid container spacing={2} sx={{ padding: '16px', paddingLeft: '32px', paddingRight: '32px' }}>
+    <main style={{ overflow: 'hidden', padding: '16px', paddingLeft: '32px', paddingRight: '32px' }}>
+      <PageTransition />
+      <header className={style.headerContainer}>
+        <Link href='/portfolio' className={style.headerLink}>PORTFOLIO</Link>
+        <Link href='/' className={style.headerLink}>HOME</Link>
+      </header>
+      <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
-          <h1 style={{ fontSize: '40px', color: 'white' }}>
+          <h1 style={{ fontSize: '40px', color: 'white', marginBottom: '12px' }}>
             {data.title}
           </h1>
           <div>
             {data.tags.map((t, i) => <Tag key={i} tag={t} />)}
           </div>
-          <a href="link">
-            Link goes here yay
+          <a href="link" className={style.linkButton}>
+            CHECK IT OUT	↗
           </a>
-          <MDXRemote source={data.description} />
+          <div className={style.mkdwnContainer}>
+            <MDXRemote source={data.description} components={MDXComponents} />
+          </div>
         </Grid>
         <Grid item xs={12} sm={6}>
           <img alt={data.title} src={data.images[0]} style={{ borderRadius: '16px' }} />
