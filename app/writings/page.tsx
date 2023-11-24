1996867f26db8c35de4a67dbfad115c4e5485083
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-11-23 23:43:20 -0600

    Finished writings

--- app/writings/page.tsx
+++ app/writings/page.tsx
@@ -4,58 +4,76 @@ import glitchStyle from "../_styles/glitch.module.css";
 import styles from "./page.module.css";
 import { Tag } from "../portfolio/_components/Tag";
 
-async function getData() {
+// This allows for server side, otherwise I would do it within useEffect
+async function getData(): Promise<Writing[]> {
   const res = await fetch('https://projk-main-page.vercel.app/api/writings')
+  if (!res.ok) throw new Error('Failed to fetch data');
+  return res.json() as Promise<Writing[]>
+}
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
+type Writing = {
+  id: number,
+  title: string,
+  icon: string,
+  tags: string[],
+  date: string,
+  link: string,
+  source: string
+};
 
-  return res.json()
+function getDateFormatted(writing: Writing) {
+  const date = new Date(writing.date);
+  let day = writing.date.substring(8, 10).padStart(1, '0'); // Months are zero-based
+  let month = writing.date.substring(5, 7); // Months are zero-based
+  let year = date.getFullYear().toString().slice(-2);
+
+  return `${day}/${month}/${year}`;
+  // 2023-02-07T00:00:00.000Z
 }
 
 export default async function Writings() {
 
-  // const data = await getData();
+  const data: Writing[] = await getData();
 
   return (
     <main style={{ overflow: 'hidden' }}>
       <PageTransition />
       <Navigation />
-      <div className={styles.titleBox}>
-        <h1 data-text={"WRITINGS"}
-          className={glitchStyle.glitch}
-          style={{ fontSize: 60 }}
-        >
-          WRITINGS
-        </h1>
+      <div className={styles.scrollBox}>
+        <div className={styles.titleBox}>
+          <h1 data-text={"WRITINGS"}
+            className={glitchStyle.glitch}
+            style={{ fontSize: 60 }}
+          >
+            WRITINGS
+          </h1>
+        </div>
+        <p style={{ color: '#CCC', textAlign: 'center', padding: '0 16px 32px 16px' }}>
+          A collection of the articles that I've written. I / Projk LLC may not have the rights to the content, as they are likely for other companies.
+        </p>
+        <section className={styles.articleContainer}>
+          {data.map((w, i) => (
+            <article className={styles.article} key={i}>
+              <a href={w.link} target="_blank" style={{ textDecoration: 'none' }}>
+                <img src={w.icon} height={60} width={60} style={{ marginRight: '16px' }} />
+              </a>
+              <div>
+                <a href={w.link} target="_blank" style={{ textDecoration: 'none' }}>
+                  <h3 className={styles.articleTitle}>{w.title}</h3>
+                </a>
+                <a href={w.link} target="_blank" style={{ textDecoration: 'none' }}>
+                  <p style={{ color: '#CCC', textDecoration: 'none', marginBottom: '8px' }}>
+                    {w.source} • {getDateFormatted(w)}
+                  </p>
+                </a>
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                  {w.tags.map((tag, t_i) => <Tag tag={tag} key={i + '_' + t_i} />)}
+                </div>
+              </div>
+            </article>
+          ))}
+        </section>
       </div>
-      <p style={{ color: 'white', textAlign: 'center', padding: '0 16px 32px 16px' }}>
-        A collection of the articles that I've written. I / Projk LLC may not have the rights to the content, as they are likely for other companies.
-      </p>
-      {/* <p>{Object.keys(data)}</p> */}
-      <section className={styles.articleContainer}>
-        <article className={styles.article}>
-          <a href="google.com" target="_blank" style={{ textDecoration: 'none' }}>
-            <img src="https://cryptologos.cc/logos/moonbeam-glmr-logo.png?v=029" height={60} width={60} style={{ marginRight: '16px' }} />
-          </a>
-          <div>
-            <a href="google.com" target="_blank" style={{ textDecoration: 'none' }}>
-              <h3 className={styles.articleTitle}>Transferring Tokens from Axelar to Centrifuge through Moonbeam’s Connected Contracts</h3>
-            </a>
-            <a href="google.com" target="_blank" style={{ textDecoration: 'none' }}>
-              <p style={{ color: '#CCC', textDecoration: 'none', marginBottom: '8px' }}>
-                Moonbeam • 12/8/22
-              </p>
-            </a>
-            <div style={{ display: 'flex', flexDirection: 'row' }}>
-              <Tag tag="Solidity" />
-              <Tag tag="Polkadot" />
-            </div>
-          </div>
-        </article>
-      </section>
     </main>
   );
 }
